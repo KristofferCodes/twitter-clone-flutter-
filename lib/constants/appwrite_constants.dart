32d6733493223e085a8afe7d7e5a3e1c6a1c2589
kristoffer <kristofferjaymes@gmail.com>
2023-04-18 10:08:31 +0100

    all boilerPlate code done

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseId = '643e520eda0ebdbb0d6b';
+  static const String projectId = '643dfbfad3dfcf6249d8';
+  static const String endpoint = 'http://localhost:80/v1';
+}
